c1d62fb0f4ee09abaebeb285090a7d42890a68d1
Jay Savaliya <jrsavalia04pc@gmail.com>
2023-03-12 17:30:49 +0530

    Updated Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,12 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import React, { useState } from "react";
 import Sliderbar from "../apifolder/Sliderbar";
-import './Slider.css';
+import "./Slider.css";
 
 const Slider = () => {
-  var length = Sliderbar.length;
-  console.log(length);
+  //   var length = Sliderbar.length;
+  //   console.log(length);
+
 
   const [slides] = useState(Sliderbar);
   const [activeSlide, setactiveSlide] = useState(0);
@@ -27,6 +28,7 @@ const Slider = () => {
       setactiveSlide(activeSlide - 1);
     }
   };
+  setInterval(nextSlide, 5000);
   return (
     <>
       <div className="parentDiv h-[540px] bg-white flex  items-center justify-between">
@@ -38,16 +40,15 @@ const Slider = () => {
           slides.map((slide, index) => {
             if (index === activeSlide) {
               return (
-                <div className="wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0]  overflow-hidden relative bg-[#ffd028]">
-                  <div className="slide flex items-center justify-center h-[100%]">
-                    <div className="forImage flex flex-1 items-center h-[100%]">
-                      <img
-                        className="h-[100%] object-cover"
-                        src={slide.src}
-                        alt="Not Found Img"
-                      />
-                    </div>
-                    <div className="des flex flex-col flex-1 place-items-start justify-center ml-11">
+                <div className="wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0]  overflow-hidden relative ">
+                  <div className="w-full h-screen">
+                    <img
+                      className="top-0 left-0 w-full h-screen object-cover"
+                      src={slide.src}
+                      alt="/"
+                    />
+                    <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
+                    <div className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                       <h2 className="text-[55px]">{slide.content.h2}</h2>
                       <p className="text-[30px]">{slide.content.p}</p>
                       <button className="btn">Shop Now</button>
