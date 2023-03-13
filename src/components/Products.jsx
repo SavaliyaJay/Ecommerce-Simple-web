8b561ebceeb02590f36815ab16ae303a38d0a344
Jay Savaliya <jrsavalia04pc@gmail.com>
2023-03-14 01:06:29 +0530

    Product

---
+++ src/components/Products.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ApiTopPropduct } from './../apifolder/ApiTopPropduct';
+import Product from './Product';
+
+const Products = () => {
+  return (
+    <>
+        <div className='flex flex-wrap p-5 items-center justify-center'>
+            {
+                ApiTopPropduct.map((product,index)=>(
+                    <Product item={product} key={index}/>
+                ))
+            }
+        </div>
+    </>
+  )
+}
+
+export default Products
